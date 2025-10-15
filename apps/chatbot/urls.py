3c9909f86b0c19ac1ab0375f288869132cc0b35f
이건희 <qwertys1347@gmail.com>
2025-10-15 11:31:47 +0900

    style: 챗봇 프로필 추가 및 CHATBOT_API_URL 환경변수 추가

--- apps/chatbot/urls.py
+++ apps/chatbot/urls.py
@@ -1,11 +1,19 @@
 from django.views.generic import TemplateView
 from django.urls import path
 
+from config import settings
+
 from .views import index
+
 
 app_name = "chatbot"
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='chatbot/index.html'), name='index'),
+    path('', TemplateView.as_view(
+            template_name='chatbot/index.html',
+            extra_context={'CHATBOT_API_URL': settings.env('CHATBOT_API_URL')}
+        ),
+        name='index'
+    ),
     path('api', index.index, name="api"),
 ]
